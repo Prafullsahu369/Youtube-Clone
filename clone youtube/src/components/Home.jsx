1a1bb56cd844cff275cb0cb7641d84dcc7c03a13
Prafullsahu369 <155sahupraful@gmail.com>
2025-06-05 17:34:06 +0530

    complete the frontend of the project

--- clone youtube/src/components/Home.jsx
+++ clone youtube/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Video from "./Video";
 import { useAuth } from "../context/Authprovider";
+import ListItems from "./ListItems";
 
 function Home() {
   const { data } = useAuth();
@@ -11,6 +12,7 @@ function Home() {
     <div className="flex">
       <Sidebar />
       <div className="h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden ">
+        <ListItems/>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-5 p-5 ">
           {data.map((items) => {
             if (items.type !== "video") return false;
---
+++ clone youtube/src/components/ListItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListItems() {
+  return (
+    <div className='flex items-center'>
+      <h1>LIstItem</h1>
+    </div>
+  )
+}
+
+export default ListItems
